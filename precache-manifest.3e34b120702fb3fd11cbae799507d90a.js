893de90d8fba6c721f4a61428d961e582924b847
Gagskaya <alisxan006@mail.ru>
2021-03-24 16:51:29 +0300

    Updates

---
+++ precache-manifest.3e34b120702fb3fd11cbae799507d90a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54431294aee36cda9c48b146d2ff0687",
+    "url": "/fligths/index.html"
+  },
+  {
+    "revision": "06fe2d2e31c04b73e826",
+    "url": "/fligths/static/css/main.d819e24d.chunk.css"
+  },
+  {
+    "revision": "b0753333e08057db5eb7",
+    "url": "/fligths/static/js/2.e65965cb.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/fligths/static/js/2.e65965cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06fe2d2e31c04b73e826",
+    "url": "/fligths/static/js/main.93a6187a.chunk.js"
+  },
+  {
+    "revision": "28d03c0bcec60a35b935",
+    "url": "/fligths/static/js/runtime-main.194c5596.js"
+  }
+]);
